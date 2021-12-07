0806e52271de3b099ea7ff8f3bf27b7cb217f809
gfijrb <gfijrb@yandex.ua>
2021-12-07 22:08:56 +0200

    add fee for eth swap

--- contracts/thirdparty/bsc/pancakeswap-V2/PancakeRouter.sol
+++ contracts/thirdparty/bsc/pancakeswap-V2/PancakeRouter.sol
@@ -386,11 +386,133 @@ interface IWETH {
     function withdraw(uint) external;
 }
 
+// SPDX-License-Identifier: MIT
+
+/**
+ * @dev Provides information about the current execution context, including the
+ * sender of the transaction and its data. While these are generally available
+ * via msg.sender and msg.data, they should not be accessed in such a direct
+ * manner, since when dealing with meta-transactions the account sending and
+ * paying for execution may not be the actual sender (as far as an application
+ * is concerned).
+ *
+ * This contract is only required for intermediate, library-like contracts.
+ */
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes memory) {
+        return msg.data;
+    }
+}
+
+/**
+ * @dev Contract module which provides a basic access control mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions.
+ *
+ * By default, the owner account will be the one that deploys the contract. This
+ * can later be changed with {transferOwnership}.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyOwner`, which can be applied to your functions to restrict their use to
+ * the owner.
+ */
+abstract contract Ownable is Context {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor() internal {
+        _setOwner(_msgSender());
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(owner() == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public virtual onlyOwner {
+        _setOwner(address(0));
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        _setOwner(newOwner);
+    }
+
+    function _setOwner(address newOwner) private {
+        address oldOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+    }
+}
+
+interface ITax {
+
+    function feeTo() external view returns (address);
+
+    function fee() external view returns (uint256);
+
+    // dest should not be zero address. if dest is contract, it should implement IERC165
+    function setFeeTo(address dest) external;
+
+    function setFeeLimit(uint256 percent) external;
+}
+
+abstract contract Tax is ITax, Ownable {
+    address public override feeTo;
+    uint256 public override fee;
+
+    uint256 public constant DECIMAL = 10_000;
+
+    event FeeToSet(address dest);
+    event FeeLimitSet(uint256 percent);
+
+    function setFeeTo(address _dest) external override onlyOwner {
+        require(_dest != address(0), "Not fee to zero");
+        feeTo = _dest;
+        emit FeeToSet(_dest);
+    }
+
+    function setFeeLimit(uint256 _percent) external override onlyOwner {
+        require(_percent <= 5_000, "No more then 50%");
+        fee = _percent;
+        emit FeeLimitSet(_percent);
+    }
+}
+
 // File: contracts\PancakeRouter.sol
 
 pragma solidity =0.6.6;
 
-contract PancakeRouter is IPancakeRouter02 {
+contract PancakeRouter is IPancakeRouter02, Tax {
     using SafeMath for uint;
 
     address public immutable override factory;
@@ -660,7 +782,11 @@ contract PancakeRouter is IPancakeRouter02 {
         );
         _swap(amounts, path, address(this));
         IWETH(WETH).withdraw(amounts[amounts.length - 1]);
-        TransferHelper.safeTransferETH(to, amounts[amounts.length - 1]);
+        uint256 toDev = fee * amounts[amounts.length - 1] / DECIMAL;
+        if (toDev > 0 && feeTo != address(0)) {
+            TransferHelper.safeTransferETH(feeTo, toDev);
+        }
+        TransferHelper.safeTransferETH(to, amounts[amounts.length - 1] - toDev);
     }
     function swapExactTokensForETH(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline)
     external
@@ -677,7 +803,11 @@ contract PancakeRouter is IPancakeRouter02 {
         );
         _swap(amounts, path, address(this));
         IWETH(WETH).withdraw(amounts[amounts.length - 1]);
-        TransferHelper.safeTransferETH(to, amounts[amounts.length - 1]);
+        uint256 toDev = fee * amounts[amounts.length - 1] / DECIMAL;
+        if (toDev > 0 && feeTo != address(0)) {
+            TransferHelper.safeTransferETH(feeTo, toDev);
+        }
+        TransferHelper.safeTransferETH(to, amounts[amounts.length - 1] - toDev);
     }
     function swapETHForExactTokens(uint amountOut, address[] calldata path, address to, uint deadline)
     external
